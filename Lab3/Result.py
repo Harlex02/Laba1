48733ffb6ef2e8b3997ff5277a9af7af0f5df10c
Harlex02 <skhrapatyy@mail.ru>
2023-03-11 11:27:35 +0300

    ГОТОВОЕ 3 лаба

---
+++ Lab3/Result.py
@@ -0,0 +1,34 @@
+import numpy
+try:
+    file1 = open("data/app.conf", "r")
+    try:
+        spisok = file1.readlines()
+        celchisl = []
+        print(spisok)
+        for i in spisok:
+            for j in i:
+                if j == " ":
+                    continue
+                else:
+                    celchisl.append(j)
+
+        celchisl = list(map(int, celchisl))
+        arr = numpy.array(celchisl)
+        print(arr)
+
+        newres = []
+
+        for i in range(len(arr)):
+            newres.append(arr[1]*i + arr[2])
+        print(newres)
+    finally:
+        file1.close()
+
+    with open("data/result.dat", "w") as file23:
+        file23.write(str(newres))
+        file23.close()
+
+except FileNotFoundError:
+    print("Невозможно открыть файл")
+except:
+    print("Ошибка при работе с файлом")
