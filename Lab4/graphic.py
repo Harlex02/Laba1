b0a035ffd46efbedd013e23882f2241785e8d216
Harlex02 <skhrapatyy@mail.ru>
2023-04-07 19:09:42 +0300

    Готовая 4 лабораторная работа

--- Lab4/graphic.py
+++ Lab4/graphic.py
@@ -1,39 +1,35 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Запрос параметров функции
-function_str = input("Введите функцию: ")
-x_min = float(input("Введите минимальное значение x: "))
-x_max = float(input("Введите максимальное значение x: "))
-step = float(input("Введите шаг вычисления: "))
-
-# Создание массива x
-x = np.arange(x_min, x_max + step, step)
-
-# Вычисление значений функции
-y = eval(function_str)
-
-# Сохранение результатов в файл
-with open("Lab4/result4.txt", "w") as file:
-    for i in range(len(x)):
-        file.write("{},{}\n".format(x[i], y[i]))
-
-# Построение графика
-plt.plot(x, y)
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("График функции")
-plt.savefig('Lab4/график.svg', format='svg')
-plt.show()
-
-
-
-# Проверка наличия файла с данными
 try:
-    with open("Lab4/result4.txt", "r") as file:
-        data = file.read()
-    print("Данные из файла:")
-    print(data)
-except FileNotFoundError:
-    pass
+    data = np.loadtxt('Lab4/graphic.txt')
+    x = data[:,0]
+    y = data[:,1]
+    plt.plot(x, y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("График функции")
+    plt.savefig('Lab4/График.svg')
+    plt.show()
 
+except FileNotFoundError:
+
+    function_str = input("Введите выражение для функции: ")
+    x_min = float(input("Введите минимальное значение аргумента: "))
+    x_max = float(input("Введите максимальное значение аргумента: "))
+    step = float(input("Введите шаг вычисления: "))
+    x = np.arange(x_min, x_max, step)
+    y = eval(function_str)
+
+    
+    with open('Lab4/graphic.txt', 'w') as f:
+        for i in range(len(x)):
+            f.write(f"{x[i]:.6f} {y[i]:.6f}\n")
+
+    
+    plt.plot(x, y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("График функции")
+    plt.savefig('Lab4/График.svg')
+    plt.show()
